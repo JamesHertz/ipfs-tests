754ae557def48df9699e22a516df3bd28e8640f7
jhertz <jh.furtado@campus.fct.unl.pt>
2023-06-29 13:55:56 +0100

    added what was missing for the experiment (now it is matter of hours to finish this)

--- images/common/start_experience.sh
+++ images/common/start_experience.sh
@@ -2,14 +2,18 @@
 
 set -e
 
-[[ -z "$MODE" ]]  && echo "MODE variable not set...." && exit 1
+# if SHARED_DIR not set also throw an error
+if [[ -z "$MODE" ]] && echo "MODE variable not set...." && exit 1
 
 # TODO: complete
 export FILES_DIR=~/files
 export LOG_DIR=~/log
 
+# SHARED_DIR=/logs (CHECK THIS)
 # generate files :)
-mkdir -p "$FILES_DIR" "$LOG_DIR"
+#mkdir -p "$FILES_DIR" "$LOG_DIR"
+
+mkdir -p "$FILES_DIR" "$LOG_DIR" "$SHARED_DIR"
 for ((i=0;i<FILE_NR;i++)) ; do
     dd bs=1M count="$FILE_SIZE" if=/dev/urandom of="$FILES_DIR/file-$i"
 done
@@ -25,5 +29,19 @@ ipfs config Discovery.MDNS.Enabled --bool false
 # start daemon
 ipfs daemon >> /dev/null 2>&1 &
 
+
 # wait a bit
 sleep 10 && ./ipfs-client "--mode=$MODE"
+
+# TODO: kill ipfs daemon
+NODE_ID=$(ipfs id --format='<id>')
+echo "nodeId: $NODE_ID"
+
+echo "Killing daemon..."
+# kill ipfs daemon the proccess
+kill $(ps | awk '{print $1, $4}' | grep ipfs | awk '{print $1}')
+
+
+for file in ${LOG_DIR}/* ; do
+    mv "$file" "$SHARED_DIR/$NODE_ID-$(basename "$file")"
+done
